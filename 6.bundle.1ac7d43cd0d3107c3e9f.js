bfb7b11900ffb6d3143ee6d6c614724d9e733278
tomieric <tomieric@gmail.com>
2016-08-28 12:40:00 +0800

    Updates

--- 6.bundle.1ac7d43cd0d3107c3e9f.js
+++
@@ -1,2 +0,0 @@
-/*! v1.0 Sun Aug 28 2016 12:36:44 GMT+0800 (CST) auth by tommyshao */
-webpackJsonp([6],{52:function(n,e,o){avalon.define({$id:"workspace",channel:"我的工作台"});n.exports={template:o(53),controller:avalon.controller("index",function(n){})}},53:function(n,e){n.exports='<div class="layer" ms-controller="workspace">\n\n  {{ channel }}\n\n</div>\n'}});
